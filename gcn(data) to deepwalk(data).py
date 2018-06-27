5f9e31d07cd29ce2d45e78919f342bd3fc98bc56
Li Zhang <32679108+LiZhang-github@users.noreply.github.com>
2018-06-27 09:03:07 +0100

    Update gcn(data) to deepwalk(data).py

--- gcn(data) to deepwalk(data).py
+++ gcn(data) to deepwalk(data).py
@@ -21,7 +21,26 @@ with open ("ind.cora.graph", 'rb') as g:
 objects_label_ty = []
 with open ("ind.cora.ty", 'rb') as ty:
     ty_data = pkl.load(ty)
-
+""" Deal with some isolated node in the graph
+    and this is refered to https://github.com/tkipf/gcn
+# Fix citeseer dataset (there are some isolated nodes in the graph)
+# Find isolated nodes, add them as zero-vecs into the right position
+with open ("ind.citeseer.y", 'rb') as y:  
+    y = pkl.load(y)  
+    
+    def parse_index_file(filename):
+        index = []
+        for line in open(filename):
+            index.append(int(line.strip()))
+        return index
+    
+    test_idx_reorder = parse_index_file("ind.citeseer.test.index")
+    test_idx_range = np.sort(test_idx_reorder)
+    test_idx_range_full = range(min(test_idx_reorder), max(test_idx_reorder)+1)
+    ty_extended = np.zeros((len(test_idx_range_full), y.shape[1]))
+    ty_extended[test_idx_range-min(test_idx_range),:]=ty_data
+    ty_data = ty_extended
+"""    
 objects_label_ally = []
 with open ("ind.cora.ally", 'rb') as ally:
     ally_data = pkl.load(ally,)
